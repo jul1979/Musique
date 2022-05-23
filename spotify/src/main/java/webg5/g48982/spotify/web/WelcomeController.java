dc75e0dcfbeeb0eddd3115496e0139019ce97861
jules ruzindana <48982@etu.he2b.be>
2022-05-23 10:55:13 +0200

    Mise en place

---
+++ spotify/src/main/java/webg5/g48982/spotify/web/WelcomeController.java
@@ -0,0 +1,15 @@
+package webg5.g48982.spotify.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
+
+
+}
---
+++ spotify/src/test/java/webg5/g48982/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package webg5.g48982.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
